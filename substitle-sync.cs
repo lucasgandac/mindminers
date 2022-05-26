89f70f668c7203a8809c67771e5cd9ba9e0f7fa0
lucasgandac <lucasgandac@hotmail.com>
2022-05-26 00:27:45 -0300

    spliting subtitle group

--- substitle-sync.cs
+++ substitle-sync.cs
@@ -21,7 +21,22 @@ class Subtitle {
                                StringSplitOptions.RemoveEmptyEntries);
         string[] stringSeparada = linhas[0].Split(Environment.NewLine);
         int numLinha = 0;
-        Console.WriteLine(text);
+        string tempo = null;
+        //Console.WriteLine(text);
+
+        foreach (string line in stringSeparada) {
+            numLinha++;
+            if(numLinha==2){
+                tempo = line;
+            }
+            Console.WriteLine(line);
+        }
+        tempo = tempo.Replace(',', '.');
+        str.Replace(',', ' ');       
+        string[] doisTempos = tempo.Split(" --> ");
+        Console.WriteLine(doisTempos[0]);
+        DateTime dateValue = DateTime.Parse(doisTempos[0]);
+
 }
     }
 }
